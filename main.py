4d455b1433918528373acc73da0aad71aba6d7e0
JanisChiffelle <janischiffelle@hotmail.ch>
2023-04-26 22:58:46 +0200

    add tests, change format data and add error handling

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from google.cloud import storage
 import requests
 import xml.etree.ElementTree as ET
 from datetime import datetime
-import pytest
 import functions_framework
 
 bucket_name = "prediswiss-raw-data"
@@ -25,7 +24,7 @@ def ingestion_data(cloud_event):
         bucket = create_bucket(bucket_name, storage_client)
 
     now = datetime.now()
-    create_blob(bucket, now.strftime("%d-%m-%Y/%H-%M"), "text/json", get_data(url=url, headers=headers))
+    create_blob(bucket, now.strftime("%Y-%m-%d/%H-%M"), "text/xml", get_data(url=url, headers=headers))
     
 def get_data(url, headers):
     payload = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:tdpplv1=\"http://datex2.eu/wsdl/TDP/Soap_Datex2/Pull/v1\" xmlns:dx223=\"http://datex2.eu/schema/2/2_0\"><SOAP-ENV:Body><dx223:d2LogicalModel xsi:type=\"dx223:D2LogicalModel\" modelBaseVersion=\"2\"><dx223:exchange xsi:type=\"dx223:Exchange\"><dx223:supplierIdentification xsi:type=\"dx223:InternationalIdentifier\"><dx223:country xsi:type=\"dx223:CountryEnum\">ch</dx223:country><dx223:nationalIdentifier xsi:type=\"dx223:String\">FEDRO</dx223:nationalIdentifier></dx223:supplierIdentification></dx223:exchange><dx223:payloadPublication xsi:type=\"dx223:GenericPublication\" lang=\"en\"><dx223:publicationCreator xsi:type=\"dx223:InternationalIdentifier\"><dx223:country xsi:type=\"dx223:CountryEnum\">ch</dx223:country><dx223:nationalIdentifier xsi:type=\"dx223:String\">FEDRO</dx223:nationalIdentifier></dx223:publicationCreator><dx223:genericPublicationName xsi:type=\"dx223:String\">MeasuredDataFilter</dx223:genericPublicationName><dx223:genericPublicationExtension xsi:type=\"dx223:_GenericPublicationExtensionType\"><dx223:measuredDataFilter xsi:type=\"dx223:MeasuredDataFilter\"><dx223:measurementSiteTableReference xsi:type=\"dx223:_MeasurementSiteTableVersionedReference\" targetClass=\"MeasurementSiteTable\" id=\"OTD:TrafficData\" version=\"0\"></dx223:measurementSiteTableReference><dx223:siteRequestReference xsi:type=\"dx223:_MeasurementSiteRecordVersionedReference\" targetClass=\"MeasurementSiteRecord\" id=\"CH:+.+\" version=\"0\"></dx223:siteRequestReference></dx223:measuredDataFilter></dx223:genericPublicationExtension></dx223:payloadPublication></dx223:d2LogicalModel></SOAP-ENV:Body></SOAP-ENV:Envelope>"
@@ -35,6 +34,8 @@ def get_data(url, headers):
         raise UrlException
     if response.status_code == 403:
         raise HeadersException
+    if response.status_code != 200:
+        raise NotSupportedException
 
     element = ET.XML(response.text)
     ET.indent(element)
@@ -60,36 +61,6 @@ class HeadersException(Exception):
     "Error in headers"
     pass
 
-class TestIngestion:
-    url1 = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/Pull"
-    url2 = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/asdPull"
-    url3 = "https://api.opentransportdata.swiss/TDP/Soap_Datex2/PullThisIsDumb"
-
-    headers1 = {
-        'Content-Type': 'text/xml; charset=utf-8',
-        'Authorization': '57c5dbbbf1fe4d0001000018dcaa101c238948f0892513258029a974',
-        'SOAPAction': 'http://opentransportdata.swiss/TDP/Soap_Datex2/Pull/v1/pullMeasuredData' 
-    }
-
-    headers2 = {
-        'Content-Type': 'text/xml; charset=utf-8',
-        'Authorization': 'asda57c5dbbbf1fe4d0001000018dcaa101c238948f0892513258029a974',
-        'SOAPAction': 'http://opentransportdata.swiss/TDP/Soap_Datex2/Pull/v1/pullMeasuredData' 
-    }
-
-    def test_get_data_url_error(self):
-        with pytest.raises(UrlException):
-            get_data(self.url2, self.headers1)
-
-    def test_get_data_correct(self):
-        assert get_data(self.url1, self.headers1) != ""
-
-    def test_get_data_after_url_correct(self):
-        try:
-            get_data(self.url3, self.headers1)
-        except UrlException:
-            assert False
-
-    def test_get_data_headers_error(self):
-        with pytest.raises(HeadersException):
-            get_data(self.url1, self.headers2) 
+class NotSupportedException(Exception):
+    "Exception not supported"
+    pass
